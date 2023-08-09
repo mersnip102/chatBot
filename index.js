d49b5d74bda4b47712cb662e83f82add50e85071
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-09 10:34:39 +0700

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     res.send("We are live")
 })
 
-app.post('/', express.json(), (req, res)=>{
+app.post('/', express.json(), async (req, res)=>{
     const agent = new dfff.WebhookClient({
         request : req,
         response : res
@@ -26,7 +26,7 @@ app.post('/', express.json(), (req, res)=>{
     function demo(agent){
         agent.add("Sending response from Webhook server as v1.1.11.1");
     }
-    function customPayloadDemo(agent){
+    async function customPayloadDemo(agent){
         var payloadData = {
             "richContent": [
               [
@@ -35,7 +35,7 @@ app.post('/', express.json(), (req, res)=>{
                   "title": "Accordion title",
                   "subtitle": "Accordion subtitle",
                   "image": {
-                    
+
                     "src": {
                       "rawUrl": "https://example.com/images/logo.png"
                     }
@@ -46,41 +46,41 @@ app.post('/', express.json(), (req, res)=>{
             ]
           }
 
-          agent.add( new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))
+          await agent.add( new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))
     }
 
-    function finalConfirmation(agent){
-      var name = agent.context.get("awaiting_name").parameters['given-name'];
-      var email = agent.context.get("awaiting_email").parameters.email;
+    // function finalConfirmation(agent){
+    //   var name = agent.context.get("awaiting_name").parameters['given-name'];
+    //   var email = agent.context.get("awaiting_email").parameters.email;
 
-      console.log(name);
-      console.log(email);
+    //   console.log(name);
+    //   console.log(email);
 
 
       
 
-      agent.add(`Hello ${name}, your email: ${email}. We confirmed your meeting.`);
+    //   agent.add(`Hello ${name}, your email: ${email}. We confirmed your meeting.`);
 
-      return db.collection('meeting').add({
-        name : name,
-        email : email,
-        time : Date.now()
-      }).then(ref =>
+    //   return db.collection('meeting').add({
+    //     name : name,
+    //     email : email,
+    //     time : Date.now()
+    //   }).then(ref =>
 
-        //fetching free slots from G-cal
-        console.log("Meeting details added to DB")
-        )
+    //     //fetching free slots from G-cal
+    //     console.log("Meeting details added to DB")
+    //     )
 
-    }
+    // }
 
 
     var intentMap = new Map();
-    intentMap.set('finalConfirmation', finalConfirmation)
+    // intentMap.set('finalConfirmation', finalConfirmation)
     intentMap.set('webhookDemo',demo )
     intentMap.set('customPayloadDemo', customPayloadDemo)
     
 
-    agent.handleRequest(intentMap);
+    await agent.handleRequest(intentMap);
 
 });
 
